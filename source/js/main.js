aa94402c3eed4e26b09927e6ee8129c3b9cf561d
mis3rably <mis3rably@gmail.com>
2023-07-12 18:16:36 +0200

    итоговая версия до код-ревью

--- source/js/main.js
+++ source/js/main.js
@@ -1,53 +1,4 @@
-import {iosVhFix} from './utils/ios-vh-fix';
-import {initModals} from './modules/modals/init-modals';
 import {Form} from './modules/form-validate/form';
-
-// ---------------------------------
-
-window.addEventListener('DOMContentLoaded', () => {
-
-  // Utils
-  // ---------------------------------
-
-  iosVhFix();
-
-  // Modules
-  // ---------------------------------
-
-  // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
-  // в load следует добавить скрипты, не участвующие в работе первого экрана
-  window.addEventListener('load', () => {
-    initModals();
-    const form = new Form();
-    window.form = form;
-    form.init();
-  });
-});
-
-// ---------------------------------
-
-// ❗❗❗ обязательно установите плагины eslint, stylelint, editorconfig в редактор кода.
-
-// привязывайте js не на классы, а на дата атрибуты (data-validate)
-
-// вместо модификаторов .block--active используем утилитарные классы
-// .is-active || .is-open || .is-invalid и прочие (обязателен нейминг в два слова)
-// .select.select--opened ❌ ---> [data-select].is-open ✅
-
-// выносим все в дата атрибуты
-// url до иконок пинов карты, настройки автопрокрутки слайдера, url к json и т.д.
-
-// для адаптивного JS используется matchMedia и addListener
-// const breakpoint = window.matchMedia(`(min-width:1024px)`);
-// const breakpointChecker = () => {
-//   if (breakpoint.matches) {
-//   } else {
-//   }
-// };
-// breakpoint.addListener(breakpointChecker);
-// breakpointChecker();
-
-// используйте .closest(el)
 
 const video = document.querySelector('.video-player__video');
 const videoButton = document.querySelector('.video-player__play-button');
@@ -55,7 +6,29 @@ const tabsButtonList = document.querySelector('.subscription__list');
 const initiallyActiveButton = tabsButtonList.querySelector('.subscription__button--active');
 const tabIndicatorFill = document.querySelector('.subscription__indicator-fill');
 const tabsBlock = document.querySelector('.subscription__wrapper');
+const tabClickableArea = document.querySelector('.faq__tabs-item');
+const tabsList = document.querySelector('.faq__tabs-list');
+const toggleTabsList = document.querySelector('.faq__buttons-list');
+const tabsGroupList = document.querySelectorAll('.faq__tabs-list');
+const tabsWrapper = document.querySelector('.faq__tabs-wrapper');
+const formMainPage = document.querySelector('.free-visit__form');
+const formSubmit = document.querySelector('.free-visit__form button[type="submit"]');
 
+const handleTabButtonClick = (clickedButton) => {
+  const tabsType = clickedButton.dataset.tabGroupButton;
+  const currentActiveButton = toggleTabsList.querySelector('.is-active');
+  const currentActiveTabGroup = tabsWrapper.querySelector('.is-shown');
+
+  currentActiveButton.classList.remove('is-active');
+  currentActiveTabGroup.classList.remove('is-shown');
+  clickedButton.classList.add('is-active');
+  for (let i = 0; i < tabsGroupList.length + 1; i++) {
+    if (tabsGroupList[i].dataset.tabGroupName === tabsType) {
+      tabsGroupList[i].classList.add('is-shown');
+      break;
+    }
+  }
+};
 
 if (video && videoButton) {
   video.removeAttribute('controls');
@@ -89,5 +62,36 @@ if (tabsButtonList && tabIndicatorFill) {
         el.classList.add('subscription-card__price-wrapper--active');
       });
     }
+  });
+}
+
+if (tabClickableArea && tabsList && toggleTabsList && tabsGroupList) {
+  handleTabButtonClick(document.querySelector('[data-tab-group-button="gym"]'));
+
+  tabsWrapper.addEventListener('click', (evt) => {
+    const tabElement = evt.target.closest('[data-tab-item="tab"]');
+    if (tabElement) {
+      const clickedTabButton = tabElement.querySelector('[data-tab-item="button"]');
+
+      clickedTabButton.classList.toggle('tab-button--active');
+      tabElement.classList.toggle('is-active');
+    }
+  });
+
+  toggleTabsList.addEventListener('click', (evt) => {
+    if (evt.target.hasAttribute('data-tab-group-button') && !evt.target.classList.contains('is-active')) {
+      handleTabButtonClick(evt.target);
+    }
+  });
+}
+
+const form = new Form();
+form.init();
+window.form = form;
+
+if (formSubmit && formMainPage) {
+  formMainPage.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+    formMainPage.submit();
   });
 }
--- source/js/vendor.js
+++ source/js/vendor.js
@@ -1,4 +1,21 @@
-// Swiper 7.4.1
-import './vendor/swiper';
-import './vendor/focus-visible-polyfill';
+import Swiper from './vendor/swiper';
 
+export const coachesSlider = new Swiper('.swiper', {
+  slidesPerView: 'auto',
+  loop: true,
+
+  navigation: {
+    nextEl: '.coaches__wrapper .swiper-button-next',
+    prevEl: '.coaches__wrapper .swiper-button-prev',
+  },
+});
+
+export const reviewsSlider = new Swiper('.reviews__slider-wrapper', {
+  slidesPerView: 1,
+  spaceBetween: 40,
+
+  navigation: {
+    nextEl: '.reviews__content-wrapper .swiper-button-next',
+    prevEl: '.reviews__content-wrapper .swiper-button-prev',
+  },
+});
