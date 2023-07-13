8c411034d24e734ed3d09eb75d3fc11a3be7e164
mis3rably <mis3rably@gmail.com>
2023-07-13 18:06:14 +0200

    контентые области заменены каскадом

--- source/js/main.js
+++ source/js/main.js
@@ -3,7 +3,7 @@ import {Form} from './modules/form-validate/form';
 const video = document.querySelector('.video-player__video');
 const videoButton = document.querySelector('.video-player__play-button');
 const tabsButtonList = document.querySelector('.subscription__list');
-const initiallyActiveButton = tabsButtonList.querySelector('.subscription__button--active');
+const initiallyActiveButton = tabsButtonList.querySelector('.subscription__item--active button');
 const tabIndicatorFill = document.querySelector('.subscription__indicator-fill');
 const tabsBlock = document.querySelector('.subscription__wrapper');
 const tabClickableArea = document.querySelector('.faq__tabs-item');
@@ -15,13 +15,14 @@ const formMainPage = document.querySelector('.free-visit__form');
 const formSubmit = document.querySelector('.free-visit__form button[type="submit"]');
 
 const handleTabButtonClick = (clickedButton) => {
-  const tabsType = clickedButton.dataset.tabGroupButton;
+  const buttonWrapper = clickedButton.closest('.faq__buttons-item');
+  const tabsType = buttonWrapper.dataset.tabGroupButton;
   const currentActiveButton = toggleTabsList.querySelector('.is-active');
   const currentActiveTabGroup = tabsWrapper.querySelector('.is-shown');
 
   currentActiveButton.classList.remove('is-active');
   currentActiveTabGroup.classList.remove('is-shown');
-  clickedButton.classList.add('is-active');
+  buttonWrapper.classList.add('is-active');
   for (let i = 0; i < tabsGroupList.length + 1; i++) {
     if (tabsGroupList[i].dataset.tabGroupName === tabsType) {
       tabsGroupList[i].classList.add('is-shown');
@@ -41,20 +42,21 @@ if (video && videoButton) {
   });
 }
 
-if (tabsButtonList && tabIndicatorFill) {
+if (tabsButtonList && tabIndicatorFill && initiallyActiveButton) {
   tabIndicatorFill.style.width = `${initiallyActiveButton.getBoundingClientRect().width}px`;
   tabIndicatorFill.style.left = `${initiallyActiveButton.offsetLeft}px`;
 
   tabsButtonList.addEventListener('click', (evt) => {
-    if (evt.target.classList.contains('subscription__button') && !evt.target.classList.contains('subscription__button--active')) {
-      const tabId = evt.target.dataset.tabButton;
+    if (evt.target.tagName === 'BUTTON' && !evt.target.closest('.subscription__item').classList.contains('subscription__item--active')) {
+      const buttonWrapper = evt.target.closest('.subscription__item');
+      const tabId = buttonWrapper.dataset.tabButton;
       const activeTabList = tabsBlock.querySelectorAll('.subscription-card__price-wrapper--active');
       const changedTabList = tabsBlock.querySelectorAll(`.subscription-card__price-wrapper[data-tab-content="${tabId}"]`);
 
-      tabsButtonList.querySelector('.subscription__button--active').classList.remove('subscription__button--active');
+      tabsButtonList.querySelector('.subscription__item--active').classList.remove('subscription__item--active');
       tabIndicatorFill.style.width = `${evt.target.getBoundingClientRect().width}px`;
       tabIndicatorFill.style.left = `${evt.target.offsetLeft}px`;
-      evt.target.classList.add('subscription__button--active');
+      buttonWrapper.classList.add('subscription__item--active');
       activeTabList.forEach((el) => {
         el.classList.remove('subscription-card__price-wrapper--active');
       });
@@ -66,7 +68,7 @@ if (tabsButtonList && tabIndicatorFill) {
 }
 
 if (tabClickableArea && tabsList && toggleTabsList && tabsGroupList) {
-  handleTabButtonClick(document.querySelector('[data-tab-group-button="gym"]'));
+  handleTabButtonClick(document.querySelector('[data-tab-group-button="gym"] button'));
 
   tabsWrapper.addEventListener('click', (evt) => {
     const tabElement = evt.target.closest('[data-tab-item="tab"]');
@@ -79,7 +81,7 @@ if (tabClickableArea && tabsList && toggleTabsList && tabsGroupList) {
   });
 
   toggleTabsList.addEventListener('click', (evt) => {
-    if (evt.target.hasAttribute('data-tab-group-button') && !evt.target.classList.contains('is-active')) {
+    if (evt.target.closest('.faq__buttons-item').hasAttribute('data-tab-group-button') && !evt.target.closest('.faq__buttons-item').classList.contains('is-active')) {
       handleTabButtonClick(evt.target);
     }
   });
